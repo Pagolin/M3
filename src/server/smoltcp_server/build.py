4bb7253c606d874b45b89c829a22722f113dcdcd
Pagolin <lisza.zeidler@tu-dresden.de>
2023-04-11 10:48:43 +0200

    remove unnecc. dependency

--- src/server/smoltcp_server/build.py
+++ src/server/smoltcp_server/build.py
@@ -1,6 +1,6 @@
 def build(gen, env):
     if env['TGT'] == 'hw':
-        libs = ['axieth', 'base', 'supc++', 'dbwrapper', 'leveldb', 'stdc++']
+        libs = ['axieth', 'supc++', 'dbwrapper', 'leveldb', 'stdc++']
     else:
         libs = ['dbwrapper', 'leveldb', 'stdc++']
     env.m3_rust_exe(gen, out = 'smoltcp_server', libs = libs, dir = 'sbin', std=True)
